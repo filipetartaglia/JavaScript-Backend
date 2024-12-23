687f3497f205532fce033ca65c38175878bfb446
Filipe Tartaglia <lipetartaglia9@gmail.com>
2024-12-23 19:49:45 -0300

    Estudando JS com foco em backend

---
+++ Tipos, var e func./escopo.js
@@ -0,0 +1,8 @@
+let estudante;
+
+if (1 > 0) {
+    estudante = 'Filipe';
+    console.log(estudante);
+}
+
+console.log(estudante);
---
+++ exercicio1.js
@@ -0,0 +1,71 @@
+/* Exercicio 1
+
+var nome = "Filipe";
+let idade = "19";
+const estudante = true;
+
+if (estudante) {
+    console.log(`Olá ${nome}, então você tem ${idade}, e é estudante, certo?`);
+} else {
+    console.log(`Olá ${nome}, então você tem ${idade}, e não é estudante, certo?`)
+}
+*/
+
+/* Exercicio 2
+
+let nome1 = "Filipe";
+let nome2 = "Tartaglia";
+let nomeCompleto = nome1 + ' ' + nome2;
+let textoNome = "Seu nome completo é " + nomeCompleto;
+
+console.log(textoNome);
+*/
+
+/* Exercicio 3
+
+let num = 19;
+let nome = "Filipe";
+let info = 'Seu nome é ' + nome + ' e tem ' + num + ' anos, correto?';
+
+console.log(info);
+*/
+
+/* Exercicio 4
+
+let nome;
+nome = 'Filipe';
+console.log(nome);
+
+nome = 'Meg'; 
+console.log(nome);
+*/
+
+/* Exercicio 5
+var nome = 'Filipe';
+
+if(nome == 'Filipe') {
+    var nome = 'Meg';
+    console.log(nome);
+}
+
+console.log(nome);
+
+let idade = 19;
+
+if(i19dade == 19) {
+    let idade = 20;
+    console.log(idade);
+}
+
+console.log(idade);
+*/
+
+/* Exercicio 6
+let chuva = true;
+
+if(chuva == false) {
+    console.log('Não precisa levar guarda-chuva.');
+} else {
+    console.log('Precisa levar guarda-chuva.');
+}
+*/
